b61933db632c83699b91eb632768e17ed797eec0
ianhi <ianhuntisaak@gmail.com>
2020-05-14 23:46:51 -0400

    first commit

---
+++ src/main.ts
@@ -0,0 +1,180 @@
+class DrawingApp {
+  private img_canvas: HTMLCanvasElement;
+  private img_context: CanvasRenderingContext2D;
+  private canvas: HTMLCanvasElement;
+  private context: CanvasRenderingContext2D;
+  private img: HTMLImageElement;
+  private lassoing: boolean;
+  private path: Path2D;
+
+  private clickX: number[] = [];
+  private clickY: number[] = [];
+
+  private colonyCount = 0;
+  private colonyCountDisplay: HTMLElement; 
+  constructor() {
+    const img_canvas = document.getElementById('img-canvas') as HTMLCanvasElement;
+    
+    const img_context = img_canvas.getContext('2d');
+    img_context.lineCap = 'round';
+    img_context.lineJoin = 'round';
+    img_context.fillStyle = 'rgba(255, 0, 0, 0.5)';
+    img_context.lineWidth = 2;
+    this.img_canvas = img_canvas;
+    this.img_context = img_context;
+    this.img_context.fillStyle = "rgba(50, 200, 100, .7)" //'#a4fae3';
+    console.log(this.img_context);
+
+    this.canvas = document.createElement('canvas');
+    this.canvas.id = 'preview';
+    this.canvas.width  = this.img_canvas.width;
+    this.canvas.height = this.img_canvas.height;
+    this.img_canvas.parentNode.appendChild(this.canvas);
+    this.context = this.canvas.getContext('2d');
+    this.context.fillStyle = "rgba(255, 0, 0, .3)" //'#a4fae3';
+    this.context.lineWidth = 2;
+    this.path = new Path2D();
+
+    this.context.imageSmoothingEnabled = true
+
+    this.colonyCountDisplay = document.getElementById('colonyCounter');
+
+    this.img = new Image();
+    this.img.src = 'colonies.png';
+    this.img.onload = (): void => {
+      this.drawImageScaled();
+    };
+
+    //this.redraw();
+    this.lassoing = false;
+    this.createUserEvents();
+  } 
+  private createUserEvents(): void {
+    this.canvas.addEventListener('mousedown', this._mouseDown);
+    this.canvas.addEventListener('mouseup', this._mouseUp);
+    this.canvas.addEventListener('mousemove', this._mouseMove);
+
+    document
+      .getElementById('clear')
+      .addEventListener('click', this.clearEventHandler);
+  }
+
+  private drawImageScaled(): void {
+    const canvas = this.img_context.canvas;
+    const img = this.img;
+    const hRatio = canvas.width / img.width;
+    const vRatio = canvas.height / img.height;
+    const ratio = Math.min(hRatio, vRatio);
+    const shiftX = (canvas.width - img.width * ratio) / 2;
+    const shiftY = (canvas.height - img.height * ratio) / 2;
+    this.img_context.drawImage(
+      img,
+      0,
+      0,
+      img.width,
+      img.height,
+      shiftX,
+      shiftY,
+      img.width * ratio,
+      img.height * ratio
+    );
+  }
+
+ // private redraw(): void {
+    //const clickX = this.clickX;
+    //const clickY = this.clickY;
+    //const context = this.context;
+
+    //for (let i = 0; i < clickX.length; ++i) {
+      //context.beginPath();
+      //context.arc(clickX[i], clickY[i], 2.5, 0, 2 * Math.PI);
+     // context.fill();
+    //}
+
+   // context.closePath();
+ // }
+
+  private addClick(x: number, y: number): void {
+    this.clickX.push(x);
+    this.clickY.push(y);
+    this.colonyCount++;
+    this.updateCounterDisplay();
+  }
+
+  private updateCounterDisplay(): void {
+    this.colonyCountDisplay.innerHTML =
+      'Colony Count : ' + this.colonyCount.toString();
+  }
+
+  private clearCanvas(): void {
+    this.drawImageScaled();
+//    this.img_context.clearRect(0, 0, this.img_canvas.width, this.img_canvas.height);
+    this.clickX = [];
+    this.clickY = [];
+  }
+
+  private canvasCoords(e: MouseEvent): [number, number]{
+    let mouseX = e.offsetX;
+    let mouseY = e.offsetY;
+    mouseX -= this.canvas.offsetLeft;
+    mouseY -= this.canvas.offsetTop;
+    return [mouseX, mouseY]
+  }
+  private clearEventHandler = (): void => {
+///    this.img_context.scale(.5,.5);
+    this.clearCanvas();
+  };
+
+  
+  private _mouseUp = (e: MouseEvent): void => {
+    if (this.lassoing = true){
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.path.closePath();
+      this.img_context.fill(this.path);
+      this.context.save();
+      this.context.fillStyle = 'rgba(100, 250, 100, 255)';
+      this.context.fill(this.path);
+      this.context.restore();
+//      this.img_context.drawImage(this.canvas, 0, 0);
+      this.lassoing = false;
+      this.context.clearRect(0,0,this.canvas.width, this.canvas.height);
+    }
+  };
+
+  private _mouseMove = (e: MouseEvent): void => {
+    console.log(this.lassoing);
+    if (this.lassoing){
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      const [mouseX, mouseY] = this.canvasCoords(e);
+      this.path.lineTo(mouseX, mouseY)
+      let closedPath = new Path2D(this.path);
+      closedPath.closePath();
+      this.context.fill(closedPath);
+      this.context.setLineDash([15, 5]);
+      this.context.stroke(this.path);
+      //this.context.lineTo(mouseX, mouseY);
+      //this.context.fill();
+      //this.context.stroke();
+
+    }
+  };
+
+  // gotta be an arrow function so we can keep on
+  // using this to refer to the Drawing rather than
+  // the clicked element
+  private _mouseDown = (e: MouseEvent): void => {
+    const [mouseX, mouseY] = this.canvasCoords(e);
+    this.path = new Path2D();
+    this.path.moveTo(mouseX,mouseY);
+    console.log('this part worked');
+    // probs need to check the mousebutton here 
+    // also if already lassoing, otherwise it possible to start multiple lassos
+    this.addClick(mouseX, mouseY);
+    this.lassoing = true;
+    //this.context.beginPath();
+    //this.context.moveTo(mouseX, mouseY);
+    //this.redraw();
+  };
+}
+
+new DrawingApp();
