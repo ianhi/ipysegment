3ed07c7652f9f226f3d7633e69d49e776c118dc4
ianhi <ianhuntisaak@gmail.com>
2020-05-30 15:55:37 -0400

    working zoom!
    trick is to keep the Sx and Sy under the mouse constant as you zoom

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,12 @@ class DrawingApp {
 
   // private colors: string[] = ['rgb(1,0,0)'];
   private maxCanvasWidth = 500;
-  private aspectRatio = 1;
+  private userZoom = 1;
+  private intrinsicZoom = 1;
+  private _Sx = 0;
+  private _Sy = 0;
+  private _sWidth = 0;
+  private _sHeight = 0;
 
   constructor() {
     this.displayCanvas = document.createElement('canvas');
@@ -43,18 +48,24 @@ class DrawingApp {
     this.img = new Image();
     this.img.src = 'colonies.png';
     this.img.onload = (): void => {
-      this.aspectRatio = this.img.width / this.img.height;
+      const aspectRatio = this.img.width / this.img.height;
+
       if (this.img.width > this.maxCanvasWidth) {
         this.displayCanvas.width = this.maxCanvasWidth;
-        this.displayCanvas.height = this.displayCanvas.width / this.aspectRatio;
+        this.displayCanvas.height = this.displayCanvas.width / aspectRatio;
+        this.intrinsicZoom = this.img.width / this.maxCanvasWidth; // / this.img.width;
       } else {
         this.displayCanvas.width = this.img.width;
         this.displayCanvas.height = this.img.height;
       }
       this.previewCanvas.width = this.displayCanvas.width;
       this.previewCanvas.height = this.displayCanvas.height;
-      this.classCanvas.width = this.displayCanvas.width;
-      this.classCanvas.height = this.displayCanvas.height;
+
+      this._sHeight = this.img.height;
+      this._sWidth = this.img.width;
+      // classCanvas should have the same size as the original image, and be drawn scaled.
+      this.classCanvas.width = this.img.width;
+      this.classCanvas.height = this.img.height;
       this.context.fillStyle = 'rgba(255, 0, 0, .3)'; //'#a4fae3';
       this.context.lineWidth = 2;
       this.classContext.fillStyle = 'rgb(0,255,255)';
@@ -76,31 +87,48 @@ class DrawingApp {
     this.previewCanvas.addEventListener('mousedown', this._mouseDown);
     this.previewCanvas.addEventListener('mouseup', this._mouseUp);
     this.previewCanvas.addEventListener('mousemove', this._mouseMove);
+    this.previewCanvas.addEventListener('wheel', this._wheel);
   }
 
   private drawImageScaled(): void {
     // should try to follow tips here: https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Optimizing_canvas
     // to not always scale the image.
-    const canvas = this.displayCanvas;
+    // const canvas = this.displayCanvas;
     const img = this.img;
-    const hRatio = canvas.width / img.width;
-    const vRatio = canvas.height / img.height;
-    const ratio = Math.min(hRatio, vRatio);
+    // const hRatio = canvas.width / img.width;
+    // const vRatio = canvas.height / img.height;
+    // const ratio = Math.min(hRatio, vRatio);
+    // console.log(this.intrinsicZoom);
+    const sWidth = this.img.width / this.userZoom;
+    const sHeight = this.img.height / this.userZoom;
+    // console.log(sWidth);
     // const shiftX = (canvas.width - img.width * ratio) / 2;
     // const shiftY = (canvas.height - img.height * ratio) / 2;
     this.displayCtx.drawImage(
       img,
-      0,
-      0,
-      img.width,
-      img.height,
-      // shiftX,
-      // shiftY,
-      0,
-      0,
-      img.width * ratio,
-      img.height * ratio
+      this._Sx, // sx
+      this._Sy, // sy
+      this._sWidth,
+      this._sHeight,
+      0, // dx
+      0, // dy
+      this.displayCanvas.width,
+      this.displayCanvas.height
     );
+    this.displayCtx.globalAlpha = 0.4;
+
+    this.displayCtx.drawImage(
+      this.classCanvas,
+      this._Sx, // sx
+      this._Sy, // sy
+      sWidth,
+      sHeight,
+      0, // dx
+      0, // dy
+      this.displayCanvas.width,
+      this.displayCanvas.height
+    );
+    this.displayCtx.globalAlpha = 1;
   }
 
   private canvasCoords(e: MouseEvent): [number, number] {
@@ -117,16 +145,30 @@ class DrawingApp {
       this.path.closePath();
       this.displayCtx.fill(this.path);
       this.context.save();
-      this.context.fillStyle = 'rgba(255, 10, 100, 55)';
+      this.context.fillStyle = 'rgb(255, 10, 100)';
       this.context.fill(this.path);
+      this.classContext.drawImage(
+        this.previewCanvas,
+        0,
+        0,
+        this.previewCanvas.width,
+        this.previewCanvas.height,
+        this._Sx,
+        this._Sy,
+        this._sWidth,
+        this._sHeight
+        // (this.previewCanvas.width * this.intrinsicZoom) / this.userZoom,
+        // (this.previewCanvas.height * this.intrinsicZoom) / this.userZoom
+      );
       this.context.restore();
       this.drawImageScaled();
       this.lassoing = false;
       // this.classContext.fillStyle = 'rgb('
-      this.classContext.fill(this.path);
+      // somehow gotta scale up the path here?
+      // this.classContext.fill(this.path);
       // this.displayCtx.drawImage(this.img, 0, 0);
       this.displayCtx.globalAlpha = 0.4;
-      this.displayCtx.drawImage(this.classCanvas, 0, 0);
+      // this.displayCtx.drawImage(this.classCanvas, 0, 0);
       // this.displayCtx.fillStyle = 'rgb(255,0,0)';
       // this.displayCtx.drawImage(this.previewCanvas, 0, 0);
       this.displayCtx.globalAlpha = 1;
@@ -135,7 +177,7 @@ class DrawingApp {
   };
 
   private _mouseMove = (e: MouseEvent): void => {
-    console.log(this.lassoing);
+    // console.log(this.lassoing);
     if (this.lassoing) {
       this.context.clearRect(0, 0, this.previewCanvas.width, this.previewCanvas.height);
       const [mouseX, mouseY] = this.canvasCoords(e);
@@ -152,12 +194,41 @@ class DrawingApp {
   // using this to refer to the Drawing rather than
   // the clicked element
   private _mouseDown = (e: MouseEvent): void => {
+    console.log(this._Sx);
     const [mouseX, mouseY] = this.canvasCoords(e);
     this.path = new Path2D();
     this.path.moveTo(mouseX, mouseY);
     // probs need to check the mousebutton here
     // also if already lassoing, otherwise it possible to start multiple lassos
     this.lassoing = true;
+  };
+
+  private _wheel = (e: MouseWheelEvent): void => {
+    // console.log(e);
+    // console.log(e.deltaY);
+    // console.log(e.offsetY);
+    const [x, y] = this.canvasCoords(e);
+    const left = (x * this.intrinsicZoom) / this.userZoom;
+    const down = (y * this.intrinsicZoom) / this.userZoom;
+    const X = this._Sx + left;
+    const Y = this._Sy + down;
+    let newLeft = left / 1.1;
+    let newDown = down / 1.1;
+    if (e.deltaY < 0) {
+      this.userZoom *= 1.1;
+      this._sWidth /= 1.1;
+      this._sHeight /= 1.1;
+    } else if (e.deltaY > 0) {
+      this.userZoom /= 1.1;
+      newLeft = left * 1.1;
+      newDown = down * 1.1;
+      this._sWidth *= 1.1;
+      this._sHeight *= 1.1;
+    }
+    this._Sx = X - newLeft;
+    this._Sy = Y - newDown;
+    this.drawImageScaled();
+    e.preventDefault();
   };
 }
 
