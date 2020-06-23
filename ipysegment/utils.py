1acf9f50490b473c7b5d68f3d56783ac7038be06
ianhi <ianhuntisaak@gmail.com>
2020-06-22 23:10:07 -0400

    use utils from ipycanvas
    https://github.com/martinRenou/ipycanvas

---
+++ ipysegment/utils.py
@@ -0,0 +1,78 @@
+"""
+This entire file was copied from ipycanvas by Martin Renou
+https://github.com/martinRenou/ipycanvas/blob/4d1fdea1b42bd963115cb881ee9d0810ef2a9913/ipycanvas/utils.py
+"""
+
+"""Binary module."""
+from io import BytesIO
+
+from PIL import Image as PILImage
+
+import numpy as np
+
+
+def image_bytes_to_array(im_bytes):
+    """Turn raw image bytes into a NumPy array."""
+    im_file = BytesIO(im_bytes)
+
+    im = PILImage.open(im_file)
+
+    return np.array(im)
+
+
+def binary_image(ar):
+    """Turn a NumPy array representing an array of pixels into a binary buffer."""
+    if ar is None:
+        return None
+    if ar.dtype != np.uint8:
+        ar = ar.astype(np.uint8)
+    if ar.ndim == 1:
+        ar = ar[np.newaxis, :]
+    if ar.ndim == 2:
+        # extend grayscale to RGBA
+        add_alpha = np.full((ar.shape[0], ar.shape[1], 4), 255, dtype=np.uint8)
+        add_alpha[:, :, :3] = np.repeat(ar[:, :, np.newaxis], repeats=3, axis=2)
+        ar = add_alpha
+    if ar.ndim != 3:
+        raise ValueError("Please supply an RGBA array with shape (width, height, 4).")
+    if ar.shape[2] != 4 and ar.shape[2] == 3:
+        add_alpha = np.full((ar.shape[0], ar.shape[1], 4), 255, dtype=np.uint8)
+        add_alpha[:, :, :3] = ar
+        ar = add_alpha
+    if not ar.flags["C_CONTIGUOUS"]:  # make sure it's contiguous
+        ar = np.ascontiguousarray(ar, dtype=np.uint8)
+    return {'shape': ar.shape, 'dtype': str(ar.dtype)}, memoryview(ar)
+
+
+def array_to_binary(ar):
+    """Turn a NumPy array into a binary buffer."""
+    # Unsupported int64 array JavaScript side
+    if ar.dtype == np.int64:
+        ar = ar.astype(np.int32)
+
+    # Unsupported float16 array JavaScript side
+    if ar.dtype == np.float16:
+        ar = ar.astype(np.float32)
+
+    # make sure it's contiguous
+    if not ar.flags["C_CONTIGUOUS"]:
+        ar = np.ascontiguousarray(ar)
+
+    return {'shape': ar.shape, 'dtype': str(ar.dtype)}, memoryview(ar)
+
+
+def populate_args(arg, args, buffers):
+    if isinstance(arg, (list, np.ndarray)):
+        arg_metadata, arg_buffer = array_to_binary(np.asarray(arg))
+        arg_metadata['idx'] = len(buffers)
+
+        args.append(arg_metadata)
+        buffers.append(arg_buffer)
+    else:
+        args.append(arg)
+
+
+def to_camel_case(snake_str):
+    """Snake case to Camel case translator."""
+    components = snake_str.split('_')
+    return components[0] + ''.join(x.title() for x in components[1:])
---
+++ src/utils.ts
@@ -0,0 +1,138 @@
+// Copyright (c) Martin Renou
+// Distributed under the terms of the Modified BSD License.
+// taken from https://github.com/martinRenou/ipycanvas/blob/4d1fdea1b42bd963115cb881ee9d0810ef2a9913/src/utils.ts
+function getTypedArray(dataview: any, metadata: any) {
+  switch (metadata.dtype) {
+    case 'int8':
+      return new Int8Array(dataview.buffer);
+      break;
+    case 'int16':
+      return new Int16Array(dataview.buffer);
+      break;
+    case 'int32':
+      return new Int32Array(dataview.buffer);
+      break;
+    case 'float32':
+      return new Float32Array(dataview.buffer);
+      break;
+    case 'float64':
+      return new Float64Array(dataview.buffer);
+      break;
+    default:
+      throw 'Unknown dtype ' + metadata.dtype;
+      break;
+  }
+}
+
+// Scalar type
+type Scalar = null | boolean | number | string;
+
+namespace Scalar {
+  export function isScalar(x: any): x is Scalar {
+    return x === null || typeof x === 'boolean' || typeof x === 'number' || typeof x === 'string';
+  }
+}
+
+type TypedArray =
+  | Int8Array
+  | Uint8Array
+  | Int16Array
+  | Uint16Array
+  | Int32Array
+  | Uint32Array
+  | Uint8ClampedArray
+  | Float32Array
+  | Float64Array;
+
+// Buffered argument
+export abstract class Arg {
+  abstract getItem(idx: number): any;
+
+  length: number;
+}
+
+class ScalarArg extends Arg {
+  constructor(value: Scalar) {
+    super();
+
+    this.value = value;
+    this.length = Infinity;
+  }
+
+  getItem(idx: number): any {
+    return this.value;
+  }
+
+  value: Scalar;
+}
+
+class BufferArg extends Arg {
+  constructor(bufferMetadata: any, buffer: any[]) {
+    super();
+
+    this.value = getTypedArray(buffer, bufferMetadata);
+    this.length = this.value.length;
+  }
+
+  getItem(idx: number): any {
+    return this.value[idx];
+  }
+
+  value: TypedArray;
+}
+
+export function getArg(metadata: any, buffers: any): Arg {
+  if (Scalar.isScalar(metadata)) {
+    return new ScalarArg(metadata);
+  }
+
+  if (metadata['idx'] !== undefined) {
+    return new BufferArg(metadata, buffers[metadata['idx']]);
+  }
+
+  throw 'Could not process argument ' + metadata;
+}
+
+export async function toBlob(canvas: HTMLCanvasElement): Promise<Blob> {
+  return new Promise<Blob>((resolve, reject) => {
+    canvas.toBlob((blob) => {
+      if (blob === null) {
+        return reject('Unable to create blob');
+      }
+
+      resolve(blob);
+    });
+  });
+}
+
+export async function toBytes(canvas: HTMLCanvasElement): Promise<Uint8ClampedArray> {
+  const blob = await toBlob(canvas);
+
+  return new Promise<Uint8ClampedArray>((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      if (typeof reader.result === 'string' || reader.result === null) {
+        return reject('Unable to read blob');
+      }
+
+      const bytes = new Uint8ClampedArray(reader.result);
+      resolve(bytes);
+    };
+    reader.readAsArrayBuffer(blob);
+  });
+}
+
+export async function fromBytes(array: Uint8ClampedArray): Promise<HTMLImageElement> {
+  const blob = new Blob([array]);
+
+  return new Promise<HTMLImageElement>((resolve, reject) => {
+    const img = new Image();
+
+    img.onload = () => {
+      resolve(img);
+    };
+
+    img.src = URL.createObjectURL(blob);
+  });
+}
